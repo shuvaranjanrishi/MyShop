bb62b64f723145f4c199093c1c2ea9c2814bd179
Shuva Ranjan Rishi <ranjan@businessaccelerate.com.bd>
2022-12-28 16:13:07 +0600

    login,register,splash done

--- app/src/main/java/com/therishideveloper/myshop/MainActivity.java
+++ app/src/main/java/com/therishideveloper/myshop/MainActivity.java
@@ -1,14 +1,50 @@
 package com.therishideveloper.myshop;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private Activity mActivity;
+    private LinearLayout loginLl, registerLl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+
+        initVariables();
+
+        initListeners();
+    }
+
+    private void initViews() {
+        loginLl = findViewById(R.id.loginLl);
+        registerLl = findViewById(R.id.registerLl);
+    }
+
+    private void initVariables() {
+        mActivity = MainActivity.this;
+    }
+
+    private void initListeners() {
+        loginLl.setOnClickListener(v -> loginLlClickAction());
+        registerLl.setOnClickListener(v -> registerLlClickAction());
+    }
+
+    private void loginLlClickAction() {
+        startActivity(new Intent(mActivity, LoginActivity.class));
+    }
+
+    private void registerLlClickAction() {
+        startActivity(new Intent(mActivity, RegisterActivity.class));
     }
 }
